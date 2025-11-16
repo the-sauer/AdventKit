4c62439a7aa14218075db27977ac67719dac6e84
Hendrik Sauer <hendrik-sauer@mailbox.org>
2025-11-16 19:43:16 +0100

    Add optional background to CalendarView.

--- Sources/AdventKit/CalendarView.swift
+++ Sources/AdventKit/CalendarView.swift
@@ -6,28 +6,36 @@
 
 import SwiftUI
 
-public struct CalendarView<DayView: View>: View {
+public struct CalendarView<DayView: View, BackgroundView: View>: View {
     @ViewBuilder let contentForDay: (Int) -> DayView
-
+    @ViewBuilder let background: () -> BackgroundView
     private let firstOfDecember: Date
     private let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 4)
 
-    public init(_ year: Int, _ contentForDay: @escaping (Int) -> DayView) {
+    public init(
+        _ year: Int,
+        _ contentForDay: @escaping (Int) -> DayView,
+        background: @escaping () -> BackgroundView = { EmptyView() }
+    ) {
         self.contentForDay = contentForDay
+        self.background = background
         self.firstOfDecember = Calendar(identifier: .gregorian)
             .date(from: DateComponents(year: year, month: 12, day: 1))!
     }
 
     public var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVGrid(columns: columns, spacing: 20) {
-                    ForEach(1...24, id: \.self) { day in
-                        DoorNavigationLink(day, contentForDay(day))
-                            .disabled(Date.now < firstOfDecember.addingTimeInterval(Double((day - 1) * 24 * 60 * 60)))
+            ZStack {
+                background()
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach(1...24, id: \.self) { day in
+                            DoorNavigationLink(day, contentForDay(day))
+                                .disabled(Date.now < firstOfDecember.addingTimeInterval(Double((day - 1) * 24 * 60 * 60)))
+                        }
                     }
+                    .padding()
                 }
-                .padding()
             }
         }
     }
@@ -36,5 +44,8 @@ public struct CalendarView<DayView: View>: View {
 #Preview {
     CalendarView(2025) { day in
         Text("Hello its day \(day)")
+    } background: {
+        Rectangle()
+            .fill(.cyan)
     }
 }
